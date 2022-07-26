33ec5b274b4587adad2f73113b551a335c3caca4
WebSmaIL <smakovskiyy@yandex.ru>
2022-07-26 18:59:30 +0600

    fix comment gen

--- commentsGen.py
+++ commentsGen.py
@@ -10,10 +10,6 @@ with open(f'./genConfig/emails.txt', 'r', encoding='utf-8') as emailsStr:
 with open(f'./genConfig/messages.txt', 'r', encoding='utf-8') as messagesStr:
     messages = messagesStr.read().split(';;;')
 
-# name = random.choice(names)
-# email = random.choice(emails)
-# message = random.choice(messages)
-
 url = "https://newsdd.ru/"
 
 try: 
@@ -31,28 +27,26 @@ urlArr = []
 for i in links:
     urlArr.append(i.get('href'))
 
-
-
 # Настраиваем чтобы не запускалось окно браузера
 option = webdriver.FirefoxOptions()
 option.headless = True
 
 browser = webdriver.Firefox()
 
-# name = random.choice(names)
-# email = random.choice(emails)
-# message = random.choice(messages)
-for i in range(100):
-    urlForComment = random.choice(urlArr)
+name = random.choice(names)
+email = random.choice(emails)
+message = random.choice(messages)
 
-    browser.get(urlForComment)
+urlForComment = random.choice(urlArr)
 
-# messageInput = browser.find_element("id", "comment")
-# authorInput = browser.find_element("id", "author")
-# emailInput = browser.find_element("id", "email")
-# submitBtn = browser.find_element("id", "submit")
+browser.get(urlForComment)
 
-# messageInput.send_keys(message)
-# authorInput.send_keys(name)
-# emailInput.send_keys(email)
-# submitBtn.click()
+messageInput = browser.find_element("id", "comment")
+authorInput = browser.find_element("id", "author")
+emailInput = browser.find_element("id", "email")
+submitBtn = browser.find_element("id", "submit")
+
+messageInput.send_keys(message)
+authorInput.send_keys(name)
+emailInput.send_keys(email)
+submitBtn.click()
